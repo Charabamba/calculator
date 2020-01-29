ae23d4ff42ebf0e5f9f441bd1ee55d2941dcaa37
Charabamba <54855818+Charabamba@users.noreply.github.com>
2020-01-29 21:30:30 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,40 @@
+let workspaceValue = "";
+
+const buttons = document.querySelectorAll(".calc-btn-common");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    workspaceValue = workspaceValue.concat(button.dataset.set);
+    console.log(workspaceValue);
+    document.querySelector(".calc-workspace").innerHTML = workspaceValue;
+  });
+});
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click", () => {
+  workspaceValue = "";
+  document.querySelector(".calc-workspace").innerHTML = workspaceValue;
+});
+const undo = document.getElementById("undo");
+undo.addEventListener("click", () => {
+  workspaceValue = workspaceValue.substring(0, workspaceValue.length - 1);
+  document.querySelector(".calc-workspace").innerHTML = workspaceValue;
+});
+
+const result = document.getElementById("result");
+function validate(str) {
+  isNaN(str[str.length - 1])
+    ? validate((str = str.substring(0, str.length - 1)))
+    : (workspaceValue = str);
+}
+function finalValidate(str) {
+  isNaN(str) ? (str = "Произошла ошибка") : str;
+  return (workspaceValue = str);
+}
+result.addEventListener("click", () => {
+  validate(workspaceValue);
+  workspaceValue = eval(workspaceValue);
+  finalValidate(workspaceValue);
+  document.querySelector(".calc-workspace").innerHTML = workspaceValue;
+  console.log(workspaceValue);
+});
